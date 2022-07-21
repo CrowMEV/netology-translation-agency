029e732f6bcf6b850934ee39a251a07c867beed0
zagz16 <varkutinov90@mail.ru>
2022-07-21 23:27:40 +0200

    бурге меню делал

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -4,8 +4,26 @@ import './order-form/order-form'
 
 
 const menuBurger=document.querySelector('.header__burger')
+
+const menuBurgerLinks=document.querySelector('.header__burger_menu')
+
 const menuBurgerActive=document.querySelector('.header__burger_active')
 
-menuBurger.addEventListener('click', function(){
+
+/* menuBurger.addEventListener('click', function(e){
+    document.body.classList.toggle("lock")
     menuBurgerActive.classList.toggle('active')
 })
+menuBurgerLinks.addEventListener('click', function(e){
+    document.body.classList.toggle("lock")
+    menuBurgerActive.classList.toggle('active')
+}) */
+
+menuBurger.onclick=()=>toggleBurger()
+menuBurgerLinks.onclick=()=>toggleBurger()
+
+function toggleBurger(){
+           document.body.classList.toggle("lock")
+        menuBurgerActive.classList.toggle('active')
+  
+}
