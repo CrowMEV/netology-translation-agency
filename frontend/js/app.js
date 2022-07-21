a899354a2328f47704b595222f7dd840d13db249
zagz16 <78856344+zagz16@users.noreply.github.com>
2022-07-21 16:54:26 +0200

    бургер

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,2 +1,10 @@
 // import 'babel-polyfill'
 import '../css/app.scss'
+
+
+const menuBurger=document.querySelector('.header__burger')
+const menuBurgerActive=document.querySelector('.header__burger_active')
+
+menuBurger.addEventListener('click', function(){
+    menuBurgerActive.classList.toggle('active')
+})
