cbebccaddc091fac13804d6673550740fdd6daa4
zagz16 <varkutinov90@mail.ru>
2022-08-02 22:20:24 +0200

    миникорректировка

--- app/src/components/contacts/flags.js
+++ app/src/components/contacts/flags.js
@@ -15,37 +15,37 @@ const flags=[
         description:'флаг чехии'
     },
     {
-        id:4,
+       
         country:'france',
         description:'флаг франции'
     },
     {
-        id:5,
+      
         country:'georgia',
         description:'флаг грузии'
     },
     {
-        id:6,
+       
         country:'germany',
         description:'флаг германии'
     },
     {
-        id:7,
+       
         country:'kazakhstan',
         description:'флаг казахстана'
     },
     {
-        id:8,
+        
         country:'kingdom',
         description:'флаг англии'
     },
     {
-        id:9,
+       
         country:'spain',
         description:'флаг испании'
     },
     {
-        id:10,
+        
         country:'usa',
         description:'флаг сша'
     },
