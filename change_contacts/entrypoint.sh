8e7469958da273292185ac5dcaea8deb1bf65d7c
Evgeny Malikov <mev@offershub.ru>
2022-10-18 19:48:14 +0500

    ssl backend

--- change_contacts/entrypoint.sh
+++ change_contacts/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Start flask dev server"
-gunicorn -b 0.0.0.0:5000 app:app
+gunicorn --certfile=/app/ssl/live/perevod22.ru/fullchain.pem --keyfile=/app/ssl/live/perevod22.ru/privkey.pem -b 0.0.0.0:5000 app:app
