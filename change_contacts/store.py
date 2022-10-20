ea376a35c8e4a1f8d4d9f19f37611116699191a0
Evgeny Malikov <mev@offershub.ru>
2022-10-20 15:54:16 +0500

    delete mongo

--- change_contacts/store.py
+++ change_contacts/store.py
@@ -1,74 +1,51 @@
+#!/usr/bin/env python
 import json
 import os
 from hashlib import sha256
-from pathlib import Path
-
-from cryptography.fernet import Fernet
 
 
-DIRECTORY_PATH = Path('store_data')
-JSON_FILE = 'data.json'
+FILE = os.path.join(os.path.dirname(__file__), 'store_data', 'data.json')
 
 
-def is_key():
-    key = ''
-    file_name = 'crypto'
-    check_file = DIRECTORY_PATH / file_name
-    if not check_file.is_file():
-        key = Fernet.generate_key()
-        with open(f'store_data/{file_name}', 'w', encoding='utf-8') as file:
-            file.write(key.decode())
-    else:
-        with open(DIRECTORY_PATH / file_name, 'r', encoding='utf-8') as file:
-            key = file.read().strip().encode()
-    return key
-
-
-fernet = Fernet(is_key())
-
-
-def encoding(string: str):
-    enstring = fernet.encrypt(string.encode())
-    return enstring
-
-
-def decoding(crypto_string):
-    destring = fernet.decrypt(crypto_string)
-    return destring
-
-
-def is_data():
+def is_data() -> None:
     data = {
-        'city': os.environ.get('CITY'),
-        'address': os.environ.get('ADDRESS'),
-        'email': os.environ.get("EMAIL"),
-        'email_password': encoding(os.environ.get("EMAIL_PASSWORD")).decode(),
-        'whatsapp': os.environ.get("WHATSAPP"),
-        'telegram': os.environ.get("TELEGRAM"),
-        'phone_1': os.environ.get("PHONE_1"),
-        'phone_2': os.environ.get("PHONE_2"),
-        'admin': sha256(os.environ.get("ADMIN").encode()).hexdigest()
+        'city': os.environ.get('CITY', ''),
+        'address': os.environ.get('ADDRESS', ''),
+        'email': os.environ.get("EMAIL", 'dfdsf'),
+        'email_password': os.environ.get("EMAIL_PASSWORD", ''),
+        'whatsapp': os.environ.get("WHATSAPP", ''),
+        'telegram': os.environ.get("TELEGRAM", ''),
+        'phone_1': os.environ.get("PHONE_1", ''),
+        'phone_2': os.environ.get("PHONE_2", ''),
     }
-
-    check_file = DIRECTORY_PATH / JSON_FILE
-    if not check_file.is_file():
-        with open(DIRECTORY_PATH / JSON_FILE, 'w', encoding='utf-8') as file:
+    if not os.path.isfile(FILE):
+        with open(FILE, 'w') as file:
             json.dump(data, file, ensure_ascii=False, indent=2)
 
 
-def read_json():
-    with open(DIRECTORY_PATH / JSON_FILE, 'w', encoding='utf-8') as file:
-        data = json.load(file)
-        del data['admin']
-        data['email_password'] = decoding(data['email_password'].encode())
-        return data
+def set_credentials() -> None:
+    credentials = (
+        sha256(os.environ.get("ADMIN", '')
+        .encode())
+        .hexdigest()
+    )
+    os.environ['ADMIN'] = credentials
 
 
-def get_admin_login_and_password():
-    with open(DIRECTORY_PATH / JSON_FILE, 'w', encoding='utf-8') as file:
-        data = json.load(file)
-        return data.get('admin', '')
+def read_json() -> dict:
+    with open(FILE) as file:
+        return json.load(file)
+
+
+def save_json(**kwargs) -> None:
+    data = read_json()
+    for key, value in kwargs:
+        data.setdefault(key, value)
+
+    with open(FILE, 'w') as file:
+        json.dump(data, file, ensure_ascii=False, indent=2)
 
 
 if __name__ == '__main__':
     is_data()
+    set_credentials()
--- change_contacts/views/views.py
+++ change_contacts/views/views.py
@@ -1,10 +1,10 @@
+import os
 from hashlib import sha256
 
 from flask import request, render_template, redirect, url_for, session, flash
 
-# from .base import get_data, save_data, get_login_and_password
+from store import read_json, save_json
 from .auth import login_required
-from .store import read_json
 
 
 def login():
@@ -15,7 +15,7 @@ def check_login():
     user_login = request.form.get('login')
     password = request.form.get('password')
     form_pass = sha256(f'{user_login}:{password}'.encode()).hexdigest()
-    hash_pass = get_login_and_password()
+    hash_pass = os.getenv('ADMIN', '')
     if form_pass != hash_pass:
         flash("Неверный логин или пароль", 'login_message')
         return redirect(url_for('login'))
@@ -25,7 +25,7 @@ def check_login():
 
 
 @login_required
-def get_from_base():
+def get_json_data():
     contacts = read_json()
     return render_template("contact.html", contacts=contacts)
 
@@ -33,7 +33,5 @@ def get_from_base():
 @login_required
 def save_to_base():
     request_data = request.form
-    passw = request_data['email_password']
-    request_data['email_password'] = sha256(passw.encode()).hexdigest()
-    save_data(request_data)
+    save_json(**request_data)
     return redirect(url_for('get_base'))
